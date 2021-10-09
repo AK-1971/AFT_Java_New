48d9886f07b154f9b000e984f70caf6cbc868e48
Andrey K <akurilo.aaa@gmail.com>
2021-10-09 14:29:12 +0300

    8.6. (Остановился на 2.30)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,18 +1,17 @@
 package ru.stqa.pft.mantis.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class RegistrationHelper {
-  private final AppsManager app;
-  private WebDriver wd;
+public class RegistrationHelper extends HelperBase{
 
   public RegistrationHelper(AppsManager app) {
-    this.app = app;
-    wd = app.getDriver(); //8.4. Ленивая инициализация
+    super(app);
   }
 
   public void start(String username, String email) {
     wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
-
+    type(By.name("username"), username);
+    type(By.name("email"), email);
   }
 }
