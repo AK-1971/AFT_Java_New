48bdc23821625fe2f3749cb8c4a0c4f694e3b288
Andrey K <akurilo.aaa@gmail.com>
2021-10-24 01:31:32 +0300

    9.6. Интеграция с GitHub. Green

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("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");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("AK-1971", "AFT_Java_New")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
