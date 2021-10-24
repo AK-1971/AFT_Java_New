68c47639517c8d89b41c35f7b2745769343e53c3
Andrey K <akurilo.aaa@gmail.com>
2021-10-24 23:13:15 +0300

    Поставил новый токен в тест

--- github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -10,7 +10,7 @@ public class GithubTests {
 
   @Test
   public void testCommits() throws IOException {
-    Github github = new RtGithub("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");
+    Github github = new RtGithub("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");
     RepoCommits commits = github.repos().get(new Coordinates.Simple("AK-1971", "AFT_Java_New")).commits();
     for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
       System.out.println(new RepoCommit.Smart(commit).message());
