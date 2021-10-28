f56a06712bca5b009f1a5c9931cc38a453716d0a
Andrey K <akurilo.aaa@gmail.com>
2021-10-28 11:49:08 +0300

    10.3. New attempt

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/AppsManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/AppsManager.java
@@ -76,7 +76,7 @@ public class AppsManager {
     } else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      //capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "Win10")));
+      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win10")));
       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
 
